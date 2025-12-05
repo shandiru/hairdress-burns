36d2b27077a2d5000c2bc80da9d15d929fa90748
shandiru <msddiru@gmail.com>
2025-12-05 20:36:00 +0530

    added

--- src/components/Homepage/ContactSection.jsx
+++ src/components/Homepage/ContactSection.jsx
@@ -244,7 +244,7 @@ const ContactSection = () => {
               >
                 Send Message
               </button>
-
+          <p class="text-xs text-center text-white" >By submitting this form, you agree to us processing your details to respond to your enquiry. Your information is handled securely and in line with our Privacy Policy.</p>
             </form>
           </div>
 
